d83562f4e398d7ae1a11c68ecf37e969c55d36e3
Hendrik Jürst <hendrik44@gmail.com>
2017-03-25 20:02:58 +0100

    Updated project to run with node 6

--- install.sh
+++ install.sh
@@ -7,47 +7,55 @@ sudo apt-get -y autoremove > /dev/null
 echo "Updating System finished"
 
 echo "install required software"
-sudo apt-get -y install vim git-core i2c-tools unzip zip
+sudo apt-get -y install vim git-core i2c-tools unzip zip  > /dev/null
 
 #install bcm2835 library for dht-sensor
 echo "install bcm2835 library for dht-sensor"
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.46.tar.gz
-tar zxvf bcm2835-1.46.tar.gz
-cd bcm2835-1.46
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.52.tar.gz
+tar zxvf bcm2835-1.52.tar.gz
+cd bcm2835-1.52
 ./configure
 make
 sudo make check 
 sudo make install
 cd
-rm -r bcm2835-1.46
-rm bcm2835-1.46.tar.gz
+rm -rf bcm2835-1.52
+rm bcm2835-1.52.tar.gz
 
 #install wiringp
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
 git pull origin
 ./build
-cd
-rm -r wiringPi
+cd ..
+rm -rf wiringPi
 
 #install nodejs
-wget http://piw.jg-bits.de/node-v0.12.7-raspberrybuild.zip
-sudo unzip node-v0.12.7-raspberrybuild.zip
-cd node-v0.12.7
-sudo make install
-cd ..
-rm -r node-v0.12.7
-rm node-v0.12.7-raspberrybuild.zip
+echo "Installing nodejs..."
+wget https://nodejs.org/dist/v6.10.1/node-v6.10.1-linux-$("arch").tar.gz
+tar -xvf node-v6.10.1-linux-$("arch").tar.gz
+cd node-v6.10.1-linux-$("arch")
+sudo cp -R * /usr/local
+rm node-v6.10.1-linux-armv6l.tar.gz
+rm -rf node-v6.10.1-linux-armv6l
 
 #install mysql
-sudo apt-get install mysql-server
-mysql -u root -p
-create database pi_weather;
-Create table data_log(time TIMESTAMP NOT NULL, temperature FLOAT NOT NULL, humidity FLOAT NOT NULL, pressure DOUBLE NOT NULL);
+echo "Installing MySQL-Database..."
+sudo apt-get install -y mysql-server
+echo "create database pi_weather" | mysql -u root -p
+echo "USE pi_weather; Create table data_log(time TIMESTAMP NOT NULL, temperature FLOAT NOT NULL, humidity FLOAT NOT NULL, pressure DOUBLE NOT NULL);" | mysql -u root -p
 
 # install node depencies
 sudo npm install pm2 -g
 sudo pm2 startup systemd -u root
+
+# Installing repo and configure server
+git clone https://github.com/Hendrik44/pi-weather-server.git
+cd pi-weather-server
+npm install
+
+echo "Please check if server runs without error and type \"node index.js\"\n"
+echo "If no error appear press CTRL-C and start it via pm2: \"sudo pm2 start index.js --name pi-weather\""
 
 #configure auto update raspbian
 #sudo apt-get install cron-apt > /dev/null
@@ -60,5 +68,5 @@ sudo pm2 startup systemd -u root
 #echo "check with: sudo cron-apt -s if it works"
 
 #dyndns
-sudo apt-get install ddclient
+#sudo apt-get install ddclient
 # https://www.ra-networks.at/2015/02/21/raspberry-pi-ddns/
